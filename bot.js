fd62073c25487d0ad4e7ea08873a78f9aaebff4b
pugmo <bradlacore@gmail.com>
2016-04-27 17:44:39 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,10 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /fuck/;
+      botRegex = /fuck/,
+      capitalFuck = /Fuck/;
 
-  if(request.text && botRegex.test(request.text)) {
+  if(request.text && botRegex.test(request.text) || capitalFuck.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
