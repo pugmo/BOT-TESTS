0ad5dc8e1e035464ac8c8c05f2692b56f1f8a4b2
pugmo <bradlacore@gmail.com>
2016-04-27 23:08:04 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,20 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /poop/;
+      botRegex = /poop/,
+      random= /shit/;
+      
 
   if(request.text && botRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage();
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+  if(request.text && random.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
