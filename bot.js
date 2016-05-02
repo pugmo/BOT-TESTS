c3a24ae9c35e692984922d68fc6ec378007c16e2
pugmo <bradlacore@gmail.com>
2016-05-01 21:53:51 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,12 +9,7 @@ function respond() {
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
-    this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
+    postMessage(bark bark);
   }
 }
 
