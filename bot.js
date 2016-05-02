6661a61e726f67ac8d79c261fad3ba55fed13317
pugmo <bradlacore@gmail.com>
2016-05-01 22:17:12 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool(ass);
+  botResponse = cool();
 
   options = {
     hostname: 'api.groupme.com',
@@ -31,7 +31,7 @@ function postMessage() {
 
   body = {
     "bot_id" : botID,
-    "text" : botResponse
+    "text" : poooop
   };
 
   console.log('sending ' + botResponse + ' to ' + botID);
