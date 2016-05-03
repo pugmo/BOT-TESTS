40107c5f60820e00988cdd5746eb6ed0514fb1b0
pugmo <bradlacore@gmail.com>
2016-05-03 02:11:01 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 var HTTPS = require('https');
-var cool = "rasd";
+var cool = "Bark Bark! :)";
 
 var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /poop/;
+      botRegex = /Good Girl/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
