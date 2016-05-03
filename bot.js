95e7b448c3faf3c874417b94db55cd3c97fe2612
pugmo <bradlacore@gmail.com>
2016-05-03 01:11:31 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 var HTTPS = require('https');
-var cool = require('cool-ascii-faces');
+var cool = "rasd";
 
 var botID = process.env.BOT_ID;
 
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = cool;
 
   options = {
     hostname: 'api.groupme.com',
